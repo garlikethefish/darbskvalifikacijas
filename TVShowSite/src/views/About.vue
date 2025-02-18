dfdbd3b34ddd1933d73c4f455b558a57fdf4b0f3
gar <cloudpoint4@gmail.com>
2025-02-18 12:38:25 +0200

    remade about page to vue

--- TVShowSite/src/views/About.vue
+++ TVShowSite/src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="app">
-      <h1>Aboyut Page</h1>
-      <p>Wasdasdsdge!</p>
+      <div class="center">
+        <h1>About Us</h1>
+        <p>This is a website dedicated to TV show reviews.<br><br>We aim to provide users with the ability to journal their favorite TV show episodes as well as view others reviews. This is such good test text about many things such as this but also a couple more.<br><br>Gloop Glop test text.</p>
+        <img class="rounded-image" :src="'./src/assets/gator.png'">
+        <p>one of our satisfied customers</p><br><br>
+      </div>
     </div>
 </template>
   
