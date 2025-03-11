77005ff5925f3a4845aa9da876cb1c8cdc9a2815
gar <cloudpoint4@gmail.com>
2025-03-11 13:27:31 +0200

    edited reviews design

--- TVShowSite/src/components/ReviewPost.vue
+++ TVShowSite/src/components/ReviewPost.vue
@@ -13,30 +13,34 @@
                 <img class="star" :src="'./src/assets/star.png'">
             </div>
         </div>
-        <div class="top-section">
-            <div class="rating">
-                <img class="star" :src="'./src/assets/star.png'">
-                <img class="star" :src="'./src/assets/star.png'">
-                <img class="star" :src="'./src/assets/star.png'">
-                <img class="star" :src="'./src/assets/star.png'">
-                <img class="star" :src="'./src/assets/star.png'">
+        <div class="right-container">
+            <div class="top-section">
+                <div class="rating">
+                    <img class="star" :src="'./src/assets/star.png'">
+                    <img class="star" :src="'./src/assets/star.png'">
+                    <img class="star" :src="'./src/assets/star.png'">
+                    <img class="star" :src="'./src/assets/star.png'">
+                    <img class="star" :src="'./src/assets/star.png'">
+                </div>
+                <div class="user">
+                    <h2>username</h2>
+                    <h3>23 reviews</h3>
+                </div>
+                    <img class="pfp" :src="'./src/assets/pfp.png'">
             </div>
-            <h2>username</h2>
-            <h3>23 reviews</h3>
-            <img class="pfp" :src="'./src/assets/pfp.png'">
-        </div>
-        <div class="content-section">
-            <h1>the best show ever</h1>
-            <p>this is the best thign ive ever seen 10/10 love it so much. wil never stop watching</p>
-        </div>
-        <div class="bottom-section">
-            <p class="date">23-04-2025</p>
-            <img class="pfp" :src="'./src/assets/pfp.png'">
-            <p class="likes">834</p>
-            <img class="pfp" :src="'./src/assets/pfp.png'">
-            <p class="dislikes">200</p>
-            <img class="pfp" :src="'./src/assets/pfp.png'">
-            <p class="comments">12</p>
+            <div class="content-section">
+                <h1>the best show ever</h1>
+                <p>this is the best thign ive ever seen 10/10 love it so much. wil never stop watching</p>
+            </div>
+            <div class="bottom-section">
+                <p class="date">23-04-2025</p>
+                <img class="pfp" :src="'./src/assets/pfp.png'">
+                <p class="likes">834</p>
+                <img class="pfp" :src="'./src/assets/pfp.png'">
+                <p class="dislikes">200</p>
+                <img class="pfp" :src="'./src/assets/pfp.png'">
+                <p class="comments">12</p>
+            </div>
         </div>
     </div>
 </template>
@@ -48,17 +52,51 @@
 .square-img{
     width: 200px;
     height: 200px;
-    object-fit: fill;
+    
+    
 }
 .star{
     /* use greyscale filter for no star */
     min-width: 30px;
+    max-width: 50px;
     height: auto;
 }
 .rating{
     display: flex;
     justify-content: center; 
 
+}
+.top-section{
+    display: flex;
+    align-items: center;
+    text-align: center;
+}
+.top-section h2, h3{
+    max-width: 20%;
+}
+.user{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 20%;
+}
+.content-section{
+    flex-direction: column;
+    min-height: 300px;
+}
+.bottom-section{
+    display: flex;
+    align-items: center;
+}
+.bottom-section p{
+    min-width: 20%;
+}
+.series-section{
+    max-width: 400px;
+    max-height: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 .review-container{
     background-color: var(--dark-bg-color);
@@ -71,10 +109,4 @@
     z-index: 1; 
     pointer-events: none; 
 }
-/* .series-section{
-
-}
-.top-section{
-
-} */
 </style>
